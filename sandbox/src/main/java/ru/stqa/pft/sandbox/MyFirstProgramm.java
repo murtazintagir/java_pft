5d1c9ff638596c4391b290df28f08aaca619f3ce
murtazintagir <tr.murtazin@yandex.ru>
2021-08-09 22:51:08 +0300

    Добавлены дополнительные классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -4,23 +4,18 @@ public class MyFirstProgramm {
 	
 	public static void main(String[] args){
 		hello("world");
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-		double a = 3;
-		double b = 4;
-		System.out.println("Периметр прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(3, 4);
+		System.out.println("Периметр прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somedody){
 		System.out.println("Hello, " + somedody + "!");
-	}
 
-	public static double area(double l){
-		return l *= l;
-	}
 
-	public static double area(double a, double b){
-		return a * b;
 	}
 
 }
