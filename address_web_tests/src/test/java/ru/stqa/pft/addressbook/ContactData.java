760c5bd113fb84f0c59c714bc617e05b5ec7dac3
murtazintagir <tr.murtazin@yandex.ru>
2021-08-13 11:20:21 +0300

    Добавлен тест создания контакта

---
+++ address_web_tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,7 @@
+package ru.stqa.pft.addressbook;
+
+public record ContactData(String first_name, String middle_name, String last_name, String nickname, String title,
+                          String company, String address, String home, String mobile, String work, String fax,
+                          String email, String email2, String email3, String homepage, String address2, String phone2,
+                          String notes, String bday, String bmonth, String byear, String aday, String amonth, String ayear) {
+}
