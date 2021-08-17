cfd09c35c18472176031374843ddd9f5b43528b7
murtazintagir <tr.murtazin@yandex.ru>
2021-08-17 15:56:52 +0300

    Добавлен тест редактирования первой группы

--- address_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ address_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -35,4 +35,12 @@ public class GroupHelper extends HelperBase {
     public void selectGroup() {
         click(By.name("selected[]"));
     }
+
+    public void initGroupEdit(String e) {
+        click(By.name(e));
+    }
+
+    public void submitGroupEdit(String u) {
+        click(By.name(u));
+    }
 }
---
+++ address_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupEditTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupEditTests extends TestBase {
+
+    @Test
+    public void testGroupEdit(){
+        app.getGroupHelper().openGroupPage("groups");
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupEdit("edit");
+        app.getGroupHelper().fillGroupForm(new GroupData("name_edit", "header_edit", "footer_edit"));
+        app.getGroupHelper().submitGroupEdit("update");
+        app.getNavigationHelper().returnToGroupPage("group page");
+    }
+}
