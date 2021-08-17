24b7fd1af1b7901423918af6533e51ce97673a13
murtazintagir <tr.murtazin@yandex.ru>
2021-08-17 14:16:55 +0300

    Исправлен тест вычисления расстояния между точками

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -11,20 +11,20 @@ public class PointTests {
     public void Point1(){
         Point p1 = new Point(7,4);
         Point p2 = new Point(13, 25);
-        Assert.assertEquals(p1.distance(p2), MyFirstProgramm.distance(p1,p2));
+        Assert.assertEquals(p1.distance(p2), 21.840329667841555);
     }
 
     @Test
     public void Point2(){
         Point p1 = new Point(2,2);
         Point p2 = new Point(8, 8);
-        Assert.assertEquals(p1.distance(p2), MyFirstProgramm.distance(p1,p2));
+        Assert.assertEquals(p1.distance(p2), 8.48528137423857);
     }
 
     @Test
     public void Point3(){
         Point p1 = new Point(10,10);
         Point p2 = new Point(8, 142);
-        Assert.assertEquals(p1.distance(p2), MyFirstProgramm.distance(p1,p2));
+        Assert.assertEquals(p1.distance(p2), 132.015150645674);
     }
 }
