e53089f24037a29babb581e88812d20ac9cd91e1
murtazintagir <tr.murtazin@yandex.ru>
2021-08-29 20:41:48 +0300

    5.5. Повсеместное использование уникальных идентификаторов объектов

--- address_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ address_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -32,8 +33,8 @@ public class GroupHelper extends HelperBase {
         click(By.name("delete"));
     }
 
-    public void select(int index) {
-        wd.findElements(By.name("selected[]")).get(index).click();
+    public void selectById(int id) {
+        wd.findElement(By.cssSelector("input[value='" + id  + "']")).click();
     }
 
     public void initGroupEdit() {
@@ -51,17 +52,16 @@ public class GroupHelper extends HelperBase {
         returnToGroupPage();
     }
 
-    public void edit(int index, GroupData group) {
-        select(index);
+    public void edit(GroupData group) {
+        selectById(group.getId());
         initGroupEdit();
         fillGroupForm(group);
         submitGroupEdit();
         returnToGroupPage();
     }
 
-
-    public void delete(int index) {
-        select(index);
+    public void delete(GroupData group) {
+        selectById(group.getId());
         del();
         returnToGroupPage();
     }
@@ -74,8 +74,8 @@ public class GroupHelper extends HelperBase {
         return wd.findElements(By.name("selected[]")).size();
     }
 
-    public List<GroupData> list() {
-        List<GroupData> groups = new ArrayList<GroupData>();
+    public Set<GroupData> all() {
+        Set<GroupData> groups = new HashSet<GroupData>();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element: elements){
             String name = element.getText();
--- address_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupEditTests.java
+++ address_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupEditTests.java
@@ -5,34 +5,30 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupEditTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
         app.goTo().groupPage();
-        if (app.group().list().size() == 0){
+        if (app.group().all().size() == 0){
             app.group().create(new GroupData().withName("name").withHeader("header").withFooter("footer"));
         }
     }
 
     @Test
     public void testGroupEdit(){
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
+        Set<GroupData> before = app.group().all();
+        GroupData editGroup = before.iterator().next();
         GroupData group = new GroupData().
-                withId(before.get(index).getId()).withName("name").withHeader("header").withFooter("footer");
-        app.group().edit(index, group);
-        List<GroupData> after = app.group().list();
+                withId(editGroup.getId()).withName("name").withHeader("header").withFooter("footer");
+        app.group().edit(group);
+        Set<GroupData> after = app.group().all();
         Assert.assertEquals(after.size(), before.size());
 
-        before.remove(before.size() - 1);
+        before.remove(editGroup);
         before.add(group);
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-        before.sort(byId);
-        after.sort(byId);
         Assert.assertEquals(before, after);
     }
 }
